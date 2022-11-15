f8d3e347e9605254d425a65811f41e9c9a7bd72c
Myo Hlaing Win <myohlingwin4486@gmail.com>
2022-11-16 00:37:27 +0630

    Updated Js files

--- js/main.js
+++ js/main.js
@@ -13,13 +13,23 @@ let list = document.querySelectorAll(".tab-list li");
 Array.from(list).forEach( x => {
     
     x.addEventListener("click", (e) => {
-        x = x.querySelector("a");
+        // x = x.querySelector("a");
         e.preventDefault();
-        document.querySelectorAll('.tab-list .active')[0].classList.remove('active')
-        x.classList.add("active")
-        let id = x.getAttribute("href");
-        document.querySelectorAll(".tab.active")[0].classList.remove('active');
-        document.querySelector(id).classList.add("active");
+        // document.querySelectorAll('.tab-list .active')[0].classList.remove('active')
+        // x.classList.add("active")
+        // let id = x.getAttribute("href");
+        // document.querySelectorAll(".tab.active")[0].classList.remove('active');
+        // document.querySelector(id).classList.add("active");
+        document.querySelectorAll(".tab-list li a").forEach(link => {
+            link.classList.remove("active");
+        })
+        x.querySelector("a").classList.add("active");
+        let id = x.querySelector("a").getAttribute("href");
+        document.querySelectorAll(".tab-container  .tab").forEach(y => {
+            y.style.display = "none";
+        })
+        document.querySelector(id).style.display = "block";
+        console.log(id)
 
     })
 })
